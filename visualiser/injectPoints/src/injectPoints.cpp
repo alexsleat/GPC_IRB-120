353071f7817d2a6e09cefa343674c4809c7f2ec9
alexsleat <alexsleat@gmail.com>
2011-11-29 17:36:57 +0000

    now taking command line arguments rather than scanf, much simpler. info in readme

--- visualiser/injectPoints/src/injectPoints.cpp
+++ visualiser/injectPoints/src/injectPoints.cpp
@@ -20,34 +20,42 @@ int main(int argc, char **argv)
 	ros::Publisher pointZMsg = n.advertise<std_msgs::Float32>("pointZ", 100);	//Z pose
 	ros::Publisher pointAMsg = n.advertise<std_msgs::Float32>("pointA", 100);	//alpha
 
+	//set up variables to be published
+
 	std_msgs::Float32 pointX;
 	std_msgs::Float32 pointY;
 	std_msgs::Float32 pointZ;
 	std_msgs::Float32 pointA;
 
-	//ros::Rate loop_rate(1); //how many times a second ros should loop
+	//get all cmd line arguments, seems to need to be int can't do float :(
+	
+	int x, y, z, a;
 
-	//while (ros::ok())
-	//{
+	//some reason, this seems to need a new nodehandle with the squiggly?
+	ros::NodeHandle nh("~");
+	nh.getParam("X", x);
+	nh.getParam("Y", y);
+	nh.getParam("Z", z);
+	nh.getParam("A", a);
 
-		ROS_INFO("Enter X: ");
-		scanf("%f", &pointX.data);
-		ROS_INFO("Enter Y: ");
-		scanf("%f", &pointY.data);
-		ROS_INFO("Enter Z: ");
-		scanf("%f", &pointZ.data);
-		ROS_INFO("Enter A: ");
-		scanf("%f", &pointA.data);
+	//since it's only taking in int, divide by 100, so 100 = 1.0, 10 = 0.1, 1 = 0.01.
 
-		pointXMsg.publish(pointX);
-		pointYMsg.publish(pointY);
-		pointZMsg.publish(pointZ);
-		pointAMsg.publish(pointA);
+	pointX.data = float(x) / 100;
+	pointY.data = float(y) / 100;
+	pointZ.data = float(z) / 100;
+	pointA.data = float(a) / 100;
 
-		//Let the world know
-		ROS_INFO("X: %f, Y: %f, Z: %f, A: %f", pointX.data, pointY.data, pointZ.data, pointA.data);
-		//Do this.
-		ros::spinOnce();
+	//publish all the things!
+
+	pointXMsg.publish(pointX);
+	pointYMsg.publish(pointY);
+	pointZMsg.publish(pointZ);
+	pointAMsg.publish(pointA);
+
+	//Let the world know
+	ROS_INFO("X: %f, Y: %f, Z: %f, A: %f", pointX.data, pointY.data, pointZ.data, pointA.data);
+	//Do this.
+	ros::spinOnce();
 	//}
 
 }
