23ffd001cbc50cc904098044b255eca15929b8f4
alexsleat <alexsleat@gmail.com>
2012-02-08 12:19:56 +0000

    client split and send 3 strings

--- irc5/server-client/python-example/client.py
+++ irc5/server-client/python-example/client.py
@@ -12,14 +12,42 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to server
 sock.connect((HOST, PORT))
 
-sock.send(data)
+# Split up the string in to 3 bits of 70
+
+#print len(data)
+count = 0;
+string1 = string2 = string3 = ''
+
+for i in data:
+	count = count + 1
+	if count <= 70: 
+		string1 = string1 + i
+	elif count <= 140:
+		string2 = string2 + i
+	else:
+		string3 = string3 + i
+
+print "String 1: " + string1
+print "String 2: " + string2
+print "String 3: " + string3
+
+send_str = [string1, string2, string3]
+
+# Send data
+
+for i in 3:
+
+	sock.send(send_str[:i])
+	received = sock.recv(1024)
+	#print "Iteration ", i, ":"
+	print "\tSent: " , data
+	print "\tRecieved: ", received	
 
 while True:
 
-	#try:
-	    # and send data
+	# and send data
 	
-	    # Receive data from the server and shut down
+	# Receive data from the server and shut down
 	received = sock.recv(1024)
 	#print "Iteration ", i, ":"
 	print "\tSent: " , data
@@ -36,4 +64,5 @@ print "fin"
 sock.send("closeSocket")
 time.sleep(1)
 sock.close()
+
 
