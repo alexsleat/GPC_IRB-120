5ebf535603a60f3f39ff5749bc50af17ec99eb3d
Gareth Griffiths <info@gareth-griffiths.co.uk>
2012-02-29 14:39:48 +0000

    Pressure detect prints the greyscale value above a certain threhold
    rostopic not yet publishing
    
    image is not being displyed, imshow I think expects an image not a Mat
    
    In this commit, I have not uploaded the build files, which are not being ignored with the .gitignore

--- tactip/pressuDetect/src/pressuDetect.cpp
+++ tactip/pressuDetect/src/pressuDetect.cpp
@@ -8,6 +8,7 @@
 namespace enc = sensor_msgs::image_encodings;
 
 static const char WINDOW[] = "Image window";
+cv::Mat mImage;
 
 class ImageConverter
 {
@@ -45,13 +46,48 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
 	}
 
 	//if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-	//	cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+		//cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
 		
 		//pressure detection code
-		cv::CvtColor(cv_ptr->image, CV_BGR2GRAY);
-		cv::Threshold(cv_ptr->image, 110, 120, CV_THRESH_BINARY);	
+		cv::cvtColor(cv_ptr->image,cv_ptr->image, CV_BGR2GRAY);
+		cv::threshold(cv_ptr->image, mImage, 110, 120, CV_THRESH_BINARY);
 		
-		cv::imshow(WINDOW, cv_ptr->image);
+		IplImage ipl_img = mImage;
+		int count = 0;
+		int x, y = 0;
+		cv::Scalar s;
+		for(x=0; x<ipl_img.height; x++){
+			for(y=0; y<ipl_img.width; y++){
+				s = cvGet2D(&ipl_img,x,y);
+				if(s.val[0] == 120){		//if we have found a black pixel
+					count++;		//increase counters
+									}
+			}
+		}
+		//count = count - 1200000;
+		printf("%d\n",count);
+		/*int whitePixels = 0;
+		int value;
+
+		for ( int i=0 ; i < 480 ; i++ )
+		{
+			//uchar* ptr = (uchar*) (cv_ptr->imageData + i * cv_ptr-> );
+			for ( int j=0 ; j < 640 ; j++ )
+			{
+				//cv::Scalar img;
+				//img = cv::get2D(cv_ptr->image, i ,j);
+				//CVImage::getpix(i,j);
+				mImage.getpix(i,j);	
+				//value = ptr[j];
+				if ( value == 120 )
+				whitePixels++;	
+			}
+		}
+		
+		whitePixels = whitePixels - 5000;
+		printf("White pixels = %d\n", whitePixels);	
+		*/
+		//cv::imshow(WINDOW, cv_ptr->image);
 		cv::waitKey(3);
     
 		image_pub_.publish(cv_ptr->toImageMsg());
@@ -61,8 +97,13 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "image_converter");
+	//ros::NodeHandle tacPressure;		//this is what ROS uses to connect to a node
 	ImageConverter ic;
 	ros::spin();
+	//ros::Publisher tacPresMsg = tacPressure.advertise<std_msgs::Float32>("tacPressure", 100);
+	//std_msgs::Float32 tacPressure; 
+	//tacPressure.data = count;
+	//tacPresMsg.publish(tacPressure);
 	return 0;
 }
 
